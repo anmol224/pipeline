20c77abf6d2837784548de6c47568e525e37d3ee
anmol jaiswal <anmolj124@gmail.com>
2023-09-04 19:25:28 +0530

    jjj

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>sc/App.js</code> and save to reload.
         </p>
         <h1>Hello Circle ci</h1>
         <h3>Hello demo</h3>
